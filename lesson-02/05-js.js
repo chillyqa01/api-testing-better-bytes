33a4527fe464e6dd99bb0ddde4834fdccaab18cf
chillyqa01 <minhphuongleqa01@gmail.com>
2025-08-25 15:16:52 +0700

    Add lesson 01 content

---
+++ lesson-02/05-js.js
@@ -0,0 +1,20 @@
+let mode = "dark";
+switch (mode) {
+    case "dark":
+        console.log("Chế độ tối đã được bật!");
+    case "light":
+        console.log("Chế độ sáng đã được bật!");
+    break;
+    default:
+        console.log("Dữ liệu không hợp lệ");
+
+};
+
+let mode = "dark";
+if (result === "dark") {
+  console.log("Chế độ tối đã được bật!");
+} else if (result === "light") {
+  console.log("Chế độ sáng đã được bật!");
+} else {
+  console.log("Dữ liệu không hợp lệ");
+}
